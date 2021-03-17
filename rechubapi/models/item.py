910cfa4d594da326a1e0eeb1a8205115060fec91
Danny McCracken <dannymccracken13@yahoo.com>
2021-03-17 13:19:03 -0500

    completed building fixtures and seeding data

--- rechubapi/models/item.py
+++ rechubapi/models/item.py
@@ -12,7 +12,7 @@ class Item(models.Model):
     name = models.CharField(max_length=25)
     user = models.ForeignKey(User, on_delete=models.CASCADE,)
     activity = models.ForeignKey(Activity, on_delete=models.DO_NOTHING)
-    container = models.ForeignKey(Container, on_delete=models.DO_NOTHING)
+    container = models.ForeignKey(Container, null=True, blank=True, on_delete=models.DO_NOTHING)
     status = models.ForeignKey(Status, on_delete=models.DO_NOTHING)
     type = models.ForeignKey(Type, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField(validators=[MinValueValidator(1)])
--- rechubapi/models/trip.py
+++ rechubapi/models/trip.py
@@ -1,14 +1,17 @@
 from django.db import models
-from django.db.models.deletion import DO_NOTHING
+from django.db.models.deletion import DO_NOTHING, CASCADE
 from rechubapi.models import Activity
 from rechubapi.models import Container
+from django.contrib.auth.models import User
+
 
 
 class Trip(models.Model):
     location = models.CharField(max_length=25)
     activity = models.ForeignKey(Activity, on_delete=models.DO_NOTHING,)
-    container = models.ForeignKey(Container, on_delete=models.DO_NOTHING)
-    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+
 
     class Meta:
         verbose_name = ("trip")
