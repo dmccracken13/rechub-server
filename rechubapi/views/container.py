8b1507d4c8222ba9272475f572e34837c363298e
Danny McCracken <dannymccracken13@yahoo.com>
2021-03-18 10:44:57 -0500

    wrote create, update, destroy functionality for containers

--- rechubapi/views/container.py
+++ rechubapi/views/container.py
@@ -1,13 +1,78 @@
 """View module for handling requests about containers"""
 from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
 from rechubapi.models import Container
 
 
 class Containers(ViewSet):
     """Rec Hub containers"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized container instance
+        """
+
+        # Create a new Python instance of the Container class
+        # and set its properties from what was sent in the
+        # body of the request from the client.
+        container = Container()
+        container.name = request.data["name"]
+        user=request.auth.user
+        container.user = user
+
+        # Try to save the new container to the database, then
+        # serialize the activity instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            container.save()
+            serializer = ContainerSerializer(container, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a container
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        container = Container.objects.get(pk=pk)
+        container.name = request.data["name"]
+        user=request.auth.user
+        container.user = user
+
+        container.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single container
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            container = Container.objects.get(pk=pk)
+            container.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Container.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single container
