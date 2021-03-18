6e563450a74fdf36d8a7f1a943acff19112b52ee
Danny McCracken <dannymccracken13@yahoo.com>
2021-03-18 11:59:34 -0500

    completed writing create, update, delete functionality for items

--- rechubapi/views/friend.py
+++ rechubapi/views/friend.py
@@ -1,13 +1,62 @@
 """View module for handling requests about friends"""
 from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
 from rechubapi.models import Friend
 
 
 class Friends(ViewSet):
     """Rec Hub friends"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized friend instance
+        """
+
+        # Create a new Python instance of the Friend class
+        # and set its properties from what was sent in the
+        # body of the request from the client.
+        friend = Friend()
+        friend.friend = request.data["friend"]
+        user=request.auth.user
+        friend.user = user
+
+        # Try to save the new friend to the database, then
+        # serialize the activity instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            friend.save()
+            serializer = FriendSerializer(friend, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single friend
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            friend = Friend.objects.get(pk=pk)
+            friend.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Friend.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single friend
--- rechubapi/views/item.py
+++ rechubapi/views/item.py
@@ -1,13 +1,96 @@
 """View module for handling requests about items"""
 from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
-from rechubapi.models import Item
+from rechubapi.models import Item, Activity, Container, Status, Type
 
 
 class Items(ViewSet):
     """Rec Hub items"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized item instance
+        """
+
+        # Create a new Python instance of the Item class
+        # and set its properties from what was sent in the
+        # body of the request from the client.
+        item = Item()
+        item.name = request.data["name"]
+        user=request.auth.user
+        item.user = user
+        activity = Activity.objects.get(pk=request.data["activity"])
+        item.activity = activity
+        container = Container.objects.get(pk=request.data["container"])
+        item.container = container
+        item_status = Status.objects.get(pk=request.data["status"])
+        item.status = item_status
+        type = Type.objects.get(pk=request.data["type"])
+        item.type = type
+        item.quantity = request.data["quantity"]
+
+        # Try to save the new activity to the database, then
+        # serialize the item instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            item.save()
+            serializer = ItemSerializer(item, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for an item
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        item = Item.objects.get(pk=pk)
+        item.name = request.data["name"]
+        user=request.auth.user
+        item.user = user
+        activity = Activity.objects.get(pk=request.data["activity"])
+        item.activity = activity
+        container = Container.objects.get(pk=request.data["container"])
+        item.container = container
+        item_status = Status.objects.get(pk=request.data["status"])
+        item.status = item_status
+        type = Type.objects.get(pk=request.data["type"])
+        item.type = type
+        item.quantity = request.data["quantity"]
+
+        item.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            item = Item.objects.get(pk=pk)
+            item.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Activity.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single item
